e0b9a19b2d2ba0fd8513d73b499d1791a767fbd4
Ankur <ankur.rangi08@gmail.com>
2024-01-08 03:48:37 +0530

    Backend - MongoDb and Models added

---
+++ backend/models/user.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minLenght: 4
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: false,
+        trim: false,
+        minLenght: 8
+    }
+}, {
+    timestamps: true,
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
